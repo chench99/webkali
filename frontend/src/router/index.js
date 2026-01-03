941b11a9b10ab5576a6b5d23efcf847b1731c2e7
chench99 <2573137977@qq.com>
2026-01-03 17:02:47 +0800

    AutoUpdate

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,16 @@ import Dashboard from '../views/Dashboard.vue'
 import WiFiPanel from '../views/WiFiPanel.vue'
 import AIAssistant from '../views/AIAssistant.vue'
 import AttackDetail from '../views/AttackDetail.vue'
-import CrackPanel from '../views/CrackPanel.vue' // [新增]
+import CrackPanel from '../views/CrackPanel.vue'
+import EvilTwinPanel from '../views/EvilTwinPanel.vue' // <--- 1. 引入新页面
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Dashboard },
   { path: '/wifi', name: 'WiFiPanel', component: WiFiPanel },
   { path: '/ai', name: 'AIAssistant', component: AIAssistant },
   { path: '/wifi/attack/:bssid', name: 'AttackDetail', component: AttackDetail },
-  { path: '/crack', name: 'CrackPanel', component: CrackPanel } // [新增]
+  { path: '/crack', name: 'CrackPanel', component: CrackPanel },
+  { path: '/eviltwin', name: 'EvilTwinPanel', component: EvilTwinPanel } // <--- 2. 添加路由
 ]
 
 const router = createRouter({
